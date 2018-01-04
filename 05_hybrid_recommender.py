81c7aca1b35c0abf596fcd8ab0b2a88e54f1b19a
Stefanie Habersatter <habi1986@gmx.at>
2018-01-04 17:39:31 +0100

    Setup

---
+++ 05_hybrid_recommender.py
@@ -0,0 +1,4 @@
+# Hybrid recommender:
+# Implement at least one way to integrate CF, CB, and PB to build a hybrid recommender (e.g., rank- based or set-based fusion).
+# Evaluate your method at least on the combinations CF+CB and CF+PB.
+
