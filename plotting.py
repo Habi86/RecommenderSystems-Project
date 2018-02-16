338ba0f43a4044fb581abdd8febc99f33cd6e90b
Daniela Moitzi <dmoitzi.mmt-m2017@fh-salzburg.ac.at>
2018-02-16 12:11:40 +0100

    add cb to plotting

--- plotting.py
+++ plotting.py
@@ -17,6 +17,9 @@ def plot_precision_recall():
     precision_rb_a = np.loadtxt('./plots/data/RB_A_precision.txt', delimiter=',')
     recall_rb_a = np.loadtxt('./plots/data/RB_A_recall.txt', delimiter=',')
 
+    precision_cb = np.loadtxt('./plots/data/CB_precision.txt', delimiter=',')
+    recall_cb = np.loadtxt('./plots/data/CB_recall.txt', delimiter=',')
+
 
     plt1 = plt.figure()
     precision_recall_plot = plt1.add_subplot(211)
@@ -25,8 +28,9 @@ def plot_precision_recall():
     precision_recall_plot.plot(recall_cf_pb, precision_cf_pb)
     precision_recall_plot.plot(recall_rb_u, precision_rb_u)
     precision_recall_plot.plot(recall_rb_a, precision_rb_a)
+    precision_recall_plot.plot(recall_cb, precision_cb)
 
-    plt.legend(['PB', 'CF', 'CF_PB', 'RB_U', 'RB_A'], loc='upper right')
+    plt.legend(['PB', 'CF', 'CF_PB', 'RB_U', 'RB_A', 'CB'], loc='upper right')
     plt1.savefig('./plots/precision-recall_neu.png')
 
 def plot_f1():
@@ -36,6 +40,7 @@ def plot_f1():
   f1_cf_pb = np.loadtxt('./plots/data/CF_PB_f1.txt', delimiter=',')
   f1_rb_u = np.loadtxt('./plots/data/RB_U_f1.txt', delimiter=',')
   f1_rb_a = np.loadtxt('./plots/data/RB_A_f1.txt', delimiter=',')
+  f1_cb = np.loadtxt('./plots/data/CB_f1.txt', delimiter=',')
 
   plt1 = plt.figure()
   f1_plot = plt1.add_subplot(211)
@@ -44,8 +49,9 @@ def plot_f1():
   f1_plot.plot(number_recommended_artists, f1_cf_pb)
   f1_plot.plot(number_recommended_artists, f1_rb_u)
   f1_plot.plot(number_recommended_artists, f1_rb_a)
+  f1_plot.plot(number_recommended_artists, f1_cb)
 
-  plt.legend(['PB', 'CF', 'CF_PB', 'RB_U', 'RB_A'], loc='upper right')
+  plt.legend(['PB', 'CF', 'CF_PB', 'RB_U', 'RB_A', 'CB'], loc='upper right')
   plt1.savefig('./plots/f1_neu.png')
 
 
